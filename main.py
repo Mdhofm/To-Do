6f632bb3bb2ac3a35275fcdcc29385e087c37b2f
mdhofm <maxhofmann1996@gmail.com>
2025-02-02 11:04:03 -0500

    Added to-do list python project

---
+++ main.py
@@ -0,0 +1,93 @@
+prompt = "Type show, add, edit or remove (type 12345 to exit program): "
+user_text = []
+
+while True:
+    user_input = input(prompt)
+    user_input = user_input.strip()
+
+    match user_input:
+        case 'add':
+            add = input("Please enter to do: ")
+            add = add.strip()
+            user_text.append(add)
+            print("Current to do list is:")
+            for text in user_text:
+                print(text)
+        case 'show' | 'display':
+            if len(user_text) == 0:
+                print("List is empty!")
+            else:
+                print("Current to do list is:")
+                for text in user_text:
+                    i = 1
+                    print(i, text)
+                    i+=1
+        case 'remove':
+            remove = input("What to do would you like to remove?")
+            remove = remove.strip()
+            if remove in user_text:
+                user_text.remove(remove)
+                print("Removed", remove, "from to do list.")
+            else:
+                print("To do does not exist in list!")
+        case 'edit':
+            user_edit = input("What item would you like to edit?")
+            
+        case '12345':
+            if len(user_text) == 0:
+                print("The list is empty...bye")
+            else:
+                print("final to do list is:")
+                for text in user_text:
+                    print(text)
+                print("\nbye bitch")
+            break
+        case whatever:
+            print("You entered an unknown command, try again.")
+
+
+    # if user_input == "add":
+    #     add = input("Please enter to do: ")
+    #     user_text.append(add)
+    #     print("Current to do list is:")
+    #     for text in user_text:
+    #         print(text)
+
+    # if user_input == "show":
+    #     if len(user_text) == 0:
+    #         print("List is empty!")
+    #     else:
+    #         print("Current to do list is:")
+    #         for text in user_text:
+    #             print(text)
+
+    # if user_input == "12345":
+    #     looping = False
+    #
+    #     if len(user_text) == 0:
+    #         print("The list is empty")
+    #     else:
+    #         print("final to do list is:")
+    #         for text in user_text:
+    #             print(text)
+
+    # if user_input == "remove":
+    #     remove = input("What to do would you like to remove?")
+    #     if remove in user_text:
+    #         user_text.remove(remove)
+    #         print("Removed", remove, "from to do list.")
+    #     else:
+    #         print("To do does not exist in list!")
+
+
+
+
+
+
+
+
+
+
+
+
+
